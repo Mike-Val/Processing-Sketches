4f2dab0f1bfc0ceb052461f7f4c1f338121ed58b
Mike_V <michelangelo.valsecchi@gmail.com>
2020-07-01 14:05:19 +0200

    Better img download

--- Puzzle_15/downloadImg.py
+++ Puzzle_15/downloadImg.py
@@ -1,9 +1,22 @@
+import random
 import sys
 sys.path.append("/usr/local/lib/python3.7/site-packages/")
 import requests
+import nekos
 
-url = sys.stdin.readline().strip()
+categories = ['feet', 'yuri', 'futanari', 'hololewd', 'lewdkemo', 'solog', 
+              'feetg', 'cum', 'erokemo', 'les', 'lewdk', 'ngif', 'tickle', 
+              'lewd', 'feed', 'gecg', 'eroyuri', 'eron', 'cum_jpg', 'bj', 
+              'solo', 'kemonomimi', 'gasm', 'poke', 'anal', 'slap', 'hentai', 
+              'avatar', 'erofeet', 'holo', 'keta', 'blowjob', 'pussy', 'tits', 'holoero', 
+              'pussy_jpg', 'pwankg', 'classic', 'kuni', 'waifu', 
+              'femdom', 'neko', 'erok', 'fox_girl', 'boobs', 
+              'smallboobs', 'ero', 'smug', 'woof']
+
+url = ''
+while url.split('.')[-1] not in ['jpg', 'jpeg']:
+    url = nekos.img(random.choice(categories))
+
 print(url)
-fileType = url.split('/')[-1].split('.')[-1]
 r = requests.get(url, allow_redirects=True)
-open(sys.stdin.readline().strip()+'downloadedImg.jpg', 'wb').write(r.content)
+open(sys.argv[1].strip()+'downloadedImg.jpg', 'wb').write(r.content)
